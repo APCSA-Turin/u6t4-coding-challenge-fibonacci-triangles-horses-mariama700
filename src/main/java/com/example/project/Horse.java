a2e292373fb19b8863f121b4520f4278d24c1a11
mariam <mariama70@nycstudents.net>
2025-01-10 07:36:19 +0000

    finishing horse lab

--- src/main/java/com/example/project/Horse.java
+++ src/main/java/com/example/project/Horse.java
@@ -1,28 +1,29 @@
-// package com.example.project;
+package com.example.project;
 
-// public class Horse  {
-//     private String name;
-//     private int weight;
+public class Horse  {
+    private String name;
+    private int weight;
 
-//     public Horse(String name, int weight) {
-//         //implement here
-//     }
+    public Horse(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
 
-//     /** @return the horse's name */
-//     public String getName() {
-//         return "";
-//     }
+    /** @return the horse's name */
+    public String getName() {
+        return name;
+    }
 
-//     /** @return the horse's weight */
-//     public int getWeight() {
-//         return 0;
-//     }
+    /** @return the horse's weight */
+    public int getWeight() {
+        return weight;
+    }
 
-//     /** @return a printable String of the Horse -> name: Cathy weight: 1500*/
-//     public String horseInfo() {
-//         return "name: " + name + ", weight: " + weight;
-//     }
-// }
+    /** @return a printable String of the Horse -> name: Cathy weight: 1500*/
+    public String horseInfo() {
+        return "name: " + name + ", weight: " + weight;
+    }
+}
 
 
  
--- src/main/java/com/example/project/HorseBarn.java
+++ src/main/java/com/example/project/HorseBarn.java
@@ -1,69 +1,92 @@
-// package com.example.project;
+package com.example.project;
 
-// public class HorseBarn {
-//     /** The stalls in the barn. Each array element holds a reference to the Horse
-//      *  that is currently occupying the stall. A null value indicates an empty stall.
-//      */
-//     private Horse[] stalls;
+public class HorseBarn {
+    /** The stalls in the barn. Each array element holds a reference to the Horse
+     *  that is currently occupying the stall. A null value indicates an empty stall.
+     */
+    private Horse[] stalls;
 
-//     /** Initializes stalls to an array with a length equal to numStalls
-//      *  (all elements remain null initially)
-//      *
-//      *  @param numStalls  the number of stalls in the barn
-//      */
-//     public HorseBarn(int numStalls) {
-//         /* to be implemented in part (a) */
-//     }
+    /** Initializes stalls to an array with a length equal to numStalls
+     *  (all elements remain null initially)
+     *
+     *  @param numStalls  the number of stalls in the barn
+     */
+    public HorseBarn(int numStalls) {
+        stalls = new Horse[numStalls];
+    }
 
-//     /** Assigns stalls to reference sampleHorses
-//      */
-//     public HorseBarn(Horse[] sampleStalls) {
-//         /* to be implemented in part (a) */
-//     }
+    /** Assigns stalls to reference sampleHorses
+     */
+    public HorseBarn(Horse[] sampleStalls) {
+        stalls = sampleStalls;
+    }
 
-//     /** Getter/accessor method for stalls
-//      *
-//      *  @return  a references to the stalls array
-//      */
-//     // to be added in part (a)
+    /** Getter/accessor method for stalls
+     *
+     *  @return  a references to the stalls array
+     */
+    // to be added in part (a)
 
-//     /** Returns a string that shows which horses are in which stalls
-//      */
-//     public String horseBarnInfo() {
-//         /* to be implemented in part (b) */
-//         return "";
-//     }
+    /** Returns a string that shows which horses are in which stalls
+     */
+    public String horseBarnInfo() {
+        String barnInfo = "";
+        for (int i = 0; i < stalls.length; i++) {
+            if (stalls[i] == null) {
+                barnInfo += "Stall " + i + ": Empty\n";
+            } else {
+                barnInfo += "Stall " + i + ": " + stalls[i].horseInfo() + "\n";
+            }
+        }
+        return barnInfo;
+    }
 
-//     /** Places a Horse into stalls at the index indicated by stall
-//      *
-//      *  PRECONDITION: stall is a valid index of stalls
-//      *
-//      *  @param horse  the Horse object to place into stalls
-//      *  @param stall  the index of stalls to place the Horse
-//      */
-//     public void placeHorse(Horse horse, int stall) {
-//         /* to be implemented in part (c) */
-//     }
+    /** Places a Horse into stalls at the index indicated by stall
+     *
+     *  PRECONDITION: stall is a valid index of stalls
+     *
+     *  @param horse  the Horse object to place into stalls
+     *  @param stall  the index of stalls to place the Horse
+     */
+    public void placeHorse(Horse horse, int stall) {
+        stalls[stall] = horse;
+    }
 
-//     /** Returns the index of the stall that contains the horse with the specified name.
-//      *
-//      * Precondition: No two horses in the barn have the same name.
-//      *
-//      * @param  name the name of the horse to find
-//      * @return  the index of the stall containing the horse with the specified name;
-//      *          -1 if no horse with the specified name is in the barn.
-//      */
-//     public int findHorseStall(String name) {
-//         /* to be implemented in part (d) */
-//         return 0;
-//     }
+    /** Returns the index of the stall that contains the horse with the specified name.
+     *
+     * Precondition: No two horses in the barn have the same name.
+     *
+     * @param  name the name of the horse to find
+     * @return  the index of the stall containing the horse with the specified name;
+     *          -1 if no horse with the specified name is in the barn.
+     */
+    public int findHorseStall(String name) {
+        for (int i = 0; i < stalls.length; i++) {
+            if (stalls[i] != null && stalls[i].getName().equals(name)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-//     /** Consolidates the barn by moving horses so that the horses are in adjacent
-//      *  stalls, starting at index 0, with no empty stall between any two horses.
-//      *
-//      *  POSTCONDITION: The order of the horses is the same as before the consolidation.
-//      */
-//     public void consolidate() {
-//         /* to be implemented in part (e) */
-//     }
-// }
+    /** Consolidates the barn by moving horses so that the horses are in adjacent
+     *  stalls, starting at index 0, with no empty stall between any two horses.
+     *
+     *  POSTCONDITION: The order of the horses is the same as before the consolidation.
+     */
+    public void consolidate() {
+        Horse[] newStalls = new Horse[stalls.length];
+        int index = 0;
+        for (Horse horse: stalls) {
+            if (horse != null) {
+                newStalls[index] = horse;
+                index++;
+            }
+        }
+        stalls = newStalls;
+    }
+
+    public Horse[] getStalls() {
+        return stalls;
+    }
+}
